15e7aebf1da8c6b05fddc6d3fa0df70e99393185
baguwix <slava2002t12@gmail.com>
2023-07-16 19:31:00 +0300

    Collections task 3

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,25 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> wordCountMap = new HashMap<>();
+
+        String[] words = sentence.toLowerCase().split("\\s+");
+
+        for (String word : words) {
+            word = word.replaceAll("[^a-zA-Z0-9]", "");
+
+            if (wordCountMap.containsKey(word)) {
+                int count = wordCountMap.get(word);
+                wordCountMap.put(word, count + 1);
+            } else {
+                wordCountMap.put(word, 1);
+            }
+        }
+
+        return wordCountMap;
     }
 }
